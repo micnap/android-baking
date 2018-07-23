09577deeb553e0e4a77361a92057ef7cf8497751
Michelle Williamson <mwill31@uis.edu>
2018-07-23 14:56:54 -0400

    Removes FloatingActionButton added automatically by AS when using Master/Detail Flow Activity

--- app/src/main/java/com/mickeywilliamson/baking/RecipeListActivity.java
+++ app/src/main/java/com/mickeywilliamson/baking/RecipeListActivity.java
@@ -7,8 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,15 +50,6 @@ public class RecipeListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         if (findViewById(R.id.recipe_detail_container) != null) {
             // The detail container view will be present only in the
